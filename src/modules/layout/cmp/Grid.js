214c33b4a7a38141e742cacdce46a6b8a9d8ecaf
Mathias Desloges <freakdev@gmail.com>
2017-09-24 20:34:58 +0200

    improve ads integration

--- src/modules/layout/cmp/Grid.js
+++ src/modules/layout/cmp/Grid.js
@@ -11,6 +11,8 @@ function getGridId() {
     return 'grid-' + ++gridInc
 }
 
+const DEFAULT_ADS_FREQ = 12
+
 class Grid extends Component {
 
     constructor(props) {
@@ -21,14 +23,52 @@ class Grid extends Component {
 
     state = {
         id: -1,
-        order: ''
+        order: '',
+        griddedData: []
     }
 
     onChangeGridOrder(order) {
         this.setState({
-            order
+            order,
+            griddedData: this.makeGriddedData(this.props.data, order)
         })
         this.props.onChangeGridOrder && this.props.onChangeGridOrder(order)
+    }
+
+    makeGriddedData(data = [], sortOrder = null) {
+        if (sortOrder) {
+            data.sort((el1, el2) => el1[this.state.order] > el2[this.state.order] ? 1 : -1)
+        }
+
+        let griddedData = []
+        let globalCount = 0,
+            lastCount = 0,
+            productCount = 0
+        while (productCount < data.length) {
+            let r = Math.floor(globalCount / this.props.cols),
+                c = globalCount % this.props.cols
+            griddedData[r] || (griddedData[r] = [])
+            if (!this.props.disableAds && productCount - lastCount === (this.props.adsFrequency || DEFAULT_ADS_FREQ)) {
+                griddedData[r][c] = { ads: true }
+                lastCount = productCount
+            } else {
+                griddedData[r][c] = data[productCount]
+                productCount++
+            }
+
+            globalCount++
+        }    
+
+        return griddedData
+
+    }
+
+    componentWillReceiveProps(nextProps) {
+        if (this.props.data !== nextProps.data) {
+            this.setState({
+                griddedData: this.makeGriddedData(nextProps.data, this.state.order)
+            })
+        }
     }
 
     componentWillMount() {
@@ -38,41 +78,35 @@ class Grid extends Component {
     }
 
     render() {
-        const { data, cols, Elmt, title, elmtClassName, rowClassName, className, adsFrequency } = this.props
-        const adsFreq = adsFrequency || 20
+        const { data, cols, Elmt, title, elmtClassName, rowClassName, className, disableAds } = this.props
         const nbCols = parseInt(cols, 10) || 5
         const gridElmtCls = ['grid-element']
+
         gridElmtCls.push('grid-' + nbCols)
         elmtClassName && gridElmtCls.push(elmtClassName)
-
-        if (this.state.order) {
-            data.sort((el1, el2) => el1[this.state.order] > el2[this.state.order] ? 1 : -1)
-        }
-
+        
         const orderOptions = data.length ? Object.keys(data[0]) : []
-
-        let griddedData = []
-        for (let r = 0; r < Math.ceil(data.length / nbCols); r++) {
-            griddedData[r] = []
-            for (let c = 0; c < nbCols; c++) {
-                griddedData[r].push(data[r * nbCols + c])
-            }
-        }
 
         return (
             <div className={ 'grid' + (className ? ' ' + className : '') }>
                 <GridHeader gridId={ this.state.id } orderOptions={ orderOptions } onChangeOrder={ this.onChangeGridOrder }>
                     <h3>{ title }</h3>
                 </GridHeader>
-                { griddedData.map((rowData, r) => (
+                { this.state.griddedData.map((rowData, r) => (
                     <div key={ this.state.id + '-' + r } className={ 'grid-row' + (rowClassName ? ' ' + rowClassName : '') }>
-                        { rowData.map( (gridElmtProps, c) => gridElmtProps && (
-                        <div className={ gridElmtCls.join(' ') } key={ this.state.id + '-' + r + '-' + c } >
-                            { ((r + c) && (r * cols + c) % adsFreq === 0) 
-                                && ( <Ads  /> ) }
-                            { React.createElement(Elmt, Object.assign(gridElmtProps)) }
-                        </div>
-                        )) }
+                        { rowData.map( (gridElmtProps, c) => 
+                            gridElmtProps 
+                                ? (
+                                    <div className={ gridElmtCls.join(' ') } key={ this.state.id + '-' + r + '-' + c } >
+                                        { !gridElmtProps.ads
+                                            ? React.createElement(Elmt, Object.assign(gridElmtProps)) 
+                                            : ( <Ads /> )
+                                        }
+                                    </div> 
+                                ) 
+                                : '' )
+                        }
+
                     </div>
                 )) }
                 <div className="clear">&nbsp;</div>
@@ -89,8 +123,9 @@ Grid.propTypes = {
     rowClassName: PropTypes.string,
     className: PropTypes.string,
     title: PropTypes.string,
-    onChangeGridOrder: PropTypes.func
-
+    onChangeGridOrder: PropTypes.func,
+    disableAds: PropTypes.bool,
+    adsFrequency:PropTypes.number
 }
 
 export default Grid
